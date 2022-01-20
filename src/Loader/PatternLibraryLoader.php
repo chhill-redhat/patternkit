4f9812ed69eb4896078c255c84bb051ed45b1f70
Dan Kolbas <dkolbas@redhat.com>
2022-01-20 10:51:22 -0700

    Issue 3259629: Updated logger classes used as they cause errors with latest drupal core upgrade 9.3.3.

--- src/Loader/PatternLibraryLoader.php
+++ src/Loader/PatternLibraryLoader.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\patternkit\Loader;
 
-use Drupal\Core\Logger\LoggerChannelInterface;
+use Psr\Log\LoggerInterface;
 use Drupal\patternkit\Asset\LibraryInterface;
 
 /**
@@ -15,13 +15,13 @@ class PatternLibraryLoader extends \Twig_Loader_Filesystem implements \Twig_Load
    *
    * @param string|array $paths
    *   Paths to pass to the Filesystem loader.
-   * @param \Drupal\Core\Logger\LoggerChannelInterface $logger
+   * @param Psr\Log\LoggerInterface $logger
    *   Logs to the patternkit channel.
    * @param \Drupal\patternkit\Asset\LibraryInterface $library
    *   Provides library names and paths.
    */
   public function __construct($paths,
-    LoggerChannelInterface $logger,
+    LoggerInterface $logger,
     LibraryInterface $library) {
     parent::__construct($paths);
     $libraries = [];
